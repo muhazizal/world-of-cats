b3e02633700e515b0aec7a78e6e54e72c05c8327
muhazizal <muhazizal.26@.com>
2022-10-16 15:08:47 +0700

    feat(master): lintfix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    "jest/globals": true,
-    node: true
+    'jest/globals': true,
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
-  ],
-  plugins: [
-    "jest"
-  ],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: ['jest'],
   // add your custom rules here
-  rules: {}
+  rules: {},
 }
