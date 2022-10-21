babe70f850a187421a6da29d4d6431412ed12bf3
muhazizal <muhazizal.26@.com>
2022-10-21 22:13:28 +0700

    feat(master): add access control allow origin

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,6 +3,7 @@ export default function ({ $axios, redirect }) {
     'x-api-key',
     'live_g17IN2EVUPV5HRtKkuCmmryXoj8wFOf2KrV1AzSjcB5KErLNYaXgBH1VawEBGJkn'
   )
+  $axios.setHeader('Access-Control-Allow-Origin', 'https://api.thecatapi.com/')
   $axios.onError((error) => {
     const status = error.response.status
     if (status === 500 || status === 400) {
